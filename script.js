ca9b99c085b4fefdf3b581c319ca8c76b58a6401
Pickles <cheesemaster56@gmail.com>
2022-02-17 06:54:40 -0500

    Working buttons, finally

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+
+rockBtn.addEventListener('click', () => handleClick('rock'));
+paperBtn.addEventListener('click', () => handleClick('paper'));
+scissorsBtn.addEventListener('click', () => handleClick('scissors'));
+
 function computerPlay() {
     const choice = Math.floor(Math.random() * 3);
     if (choice === 0) return "rock";
@@ -5,37 +13,31 @@ function computerPlay() {
     else if (choice === 2) return "scissors";
 }
 
-function game() {
-    function playRound(playerSelection, computerSelection) {
-        while (playerSelection === computerSelection) {
-            console.log("Same choice, trying again.");
-            computerSelection = computerPlay();
-        }
-
-        if (playerSelection === "rock") {
-            if (computerSelection === "paper") return "Paper beats rock, computer wins!";
-            if (computerSelection === "scissors") return "Rock beats scissors, player wins!";
-        }
-
-        if (playerSelection === "paper") {
-            if (computerSelection === "rock") return "Paper beats rock, player wins!";
-            if (computerSelection === "scissors") return "Scissors beats paper, computer wins!";
-        }
-
-        if (playerSelection === "scissors") {
-            if (computerSelection === "paper") return "Scissors beats paper, player wins!";
-            if (computerSelection === "rock") return "Rock beats scissors, computer wins!";
-        }
+function playRound(playerSelection, computerSelection) {
+    while (playerSelection === computerSelection) {
+        console.log("Same choice, trying again.");
+        computerSelection = computerPlay();
     }
-    /*
-    for (let i=0; i<5; i++) {
-        const playerSelection = prompt("rock, paper, or scissors?");
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection,computerSelection));
-    }*/
-    const playerSelection = prompt("rock, paper, or scissors?");
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection,computerSelection));
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") return "Paper beats rock, computer wins!";
+        if (computerSelection === "scissors") return "Rock beats scissors, player wins!";
+    }
+
+    if (playerSelection === "paper") {
+        if (computerSelection === "rock") return "Paper beats rock, player wins!";
+        if (computerSelection === "scissors") return "Scissors beats paper, computer wins!";
+    }
+
+    if (playerSelection === "scissors") {
+        if (computerSelection === "paper") return "Scissors beats paper, player wins!";
+        if (computerSelection === "rock") return "Rock beats scissors, computer wins!";
+    }
 }
 
-game();
+function handleClick(playerSelection) {
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    console.log(playerSelection);
+    console.log(playRound(playerSelection,computerSelection));
+}
